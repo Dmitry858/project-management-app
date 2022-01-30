a0744caf43b5fde33cbd45f6674fdfec89fd9a1c
Dmitry <frontender@inbox.ru>
2022-01-30 13:01:25 +0400

    Edit title view

--- resources/views/include/title.blade.php
+++ resources/views/include/title.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-800">
     <div class="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
-        <h1 class="font-bold pl-2">Заголовок страницы</h1>
+        <h1 class="font-bold pl-2">{{ $title ?? 'Менеджер проектов' }}</h1>
     </div>
 </div>
