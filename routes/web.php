0a9006d393daea3128ac18ee9aafaaf34f859b97
Dmitry <frontender@inbox.ru>
2022-01-31 17:26:28 +0400

    Edit web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProfileController;
 
 /*
@@ -23,5 +24,7 @@ Route::middleware(['auth'])->group(function() {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('/projects', ProjectController::class);
     Route::resource('/tasks', TaskController::class);
+    Route::resource('/members', MemberController::class);
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
