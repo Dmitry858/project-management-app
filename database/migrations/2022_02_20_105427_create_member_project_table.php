892f7e2ed7e259671f2cbfbc3b1ca92dac1a2c13
Dmitry <frontender@inbox.ru>
2022-02-20 15:01:01 +0400

    Add create_member_project_table migration

---
+++ database/migrations/2022_02_20_105427_create_member_project_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_project', function (Blueprint $table) {
+            $table->id();
+            $table->integer('member_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_project');
+    }
+}
