614f651090b97f3546018fa8ddfe3c71ccadee79
Dmitry <frontender@inbox.ru>
2022-02-20 14:20:31 +0400

    Create members view

--- resources/views/include/buttons/create.blade.php
+++ resources/views/include/buttons/create.blade.php
@@ -1,3 +1,7 @@
 <a href="{{ $link }}" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-    @lang('buttons.create')
+    @if (isset($label))
+        @lang('buttons.'.$label)
+    @else
+        @lang('buttons.create')
+    @endif
 </a>
---
+++ resources/views/members/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('include.flash-success')
+    @include('include.flash-error')
+
+    @if (count($members) > 0)
+        <div class="flex flex-col">
+            <div class="overflow-x-auto">
+                <div class="py-2 inline-block min-w-full">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full">
+                            <thead class="bg-blue-300 border-b">
+                                <tr>
+                                    @include('include.table-th', ['text' => 'ID'])
+                                    @include('include.table-th', ['text' => 'Имя'])
+                                    @include('include.table-th', ['text' => 'Проекты'])
+                                    @include('include.table-th', ['text' => ''])
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($members as $member)
+                                    <tr class="bg-white border-b">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            {{ $member->id }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{ $member->user->name }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            0
+                                        </td>
+                                        <td class="flex text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            @include('include.buttons.edit', ['link' => route('members.edit', ['member' => $member->id])])
+                                            @include('include.buttons.delete', ['link' => route('members.destroy', ['member' => $member->id])])
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @else
+        <p>@lang('empty.members')</p>
+    @endif
+
+    <div class="py-4 mt-4">
+        @include('include.buttons.create', ['link' => route('members.create'), 'label' => 'add'])
+    </div>
+@endsection
+
