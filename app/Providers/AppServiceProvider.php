70b76099b3c0f75fabf9b7f7d521964021f364f1
Dmitry <frontender@inbox.ru>
2022-02-24 17:58:59 +0400

    Add binding in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Interfaces\ProjectRepositoryInterface;
+use App\Repositories\EloquentProjectRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(ProjectRepositoryInterface::class, EloquentProjectRepository::class);
     }
 }
