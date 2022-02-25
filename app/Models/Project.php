d8ccba87e97cf20f5f11fbd77c49c413c171501b
Dmitry <frontender@inbox.ru>
2022-02-25 14:52:09 +0400

    Edit Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,4 +14,9 @@ class Project extends Model
         'description',
         'is_active',
     ];
+
+    public function members()
+    {
+        return $this->belongsToMany(Member::class);
+    }
 }
