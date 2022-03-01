6649e78742532b5c9f7e8f20bda867180295d9ad
Dmitry <frontender@inbox.ru>
2022-03-01 14:40:20 +0400

    Create EloquentUserRepository

---
+++ app/Repositories/EloquentUserRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Models\User;
+
+class EloquentUserRepository implements UserRepositoryInterface
+{
+    public function find(int $id)
+    {
+        return User::find($id);
+    }
+
+    public function search(array $filter = [], bool $withPaginate = true)
+    {
+        if ($withPaginate)
+        {
+            return User::where($filter)->paginate();
+        }
+        else
+        {
+            return User::where($filter)->get();
+        }
+    }
+
+    public function createFromArray(array $data)
+    {
+        $user = User::create($data);
+
+        return $user;
+    }
+
+    public function updateFromArray(int $id, array $data): bool
+    {
+        $user = $this->find($id);
+
+        return $user ? $user->update($data) : false;
+    }
+
+    public function delete(int $id): bool
+    {
+        $user = $this->find($id);
+
+        return $user ? $user->delete() : false;
+    }
+}
