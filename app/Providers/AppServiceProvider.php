83b0bd08898728d71a58aa11ea856cfbef36ed3f
Dmitry <frontender@inbox.ru>
2022-03-02 13:01:00 +0400

    Edit AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Interfaces\ProjectRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\EloquentProjectRepository;
+use App\Repositories\EloquentUserRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(ProjectRepositoryInterface::class, EloquentProjectRepository::class);
+
+        $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
     }
 }
