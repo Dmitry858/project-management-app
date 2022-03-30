36d87e1a079794dba17a587680ef42e4a8565735
Dmitry <frontender@inbox.ru>
2022-03-30 10:42:53 +0400

    Add the method photoSrc() to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -84,5 +86,17 @@ class User extends Authenticatable
         }
 
         return false;
+    }
+
+    public function photoSrc(): string
+    {
+        $src = '';
+        if ($this->photo && Storage::exists($this->photo))
+        {
+            $ext = File::extension($this->photo);
+            $src = 'data:image/'.$ext.';base64,'.base64_encode(Storage::get($this->photo));
+        }
+
+        return $src;
     }
 }
