14b735d442184cc60e2126ca0234fdb1b3efac0f
Dmitry <frontender@inbox.ru>
2022-07-22 16:44:18 +0400

    Add create_tasks_table migration

---
+++ database/migrations/2022_07_22_114521_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->string('description')->nullable();
+            $table->integer('owner_id');
+            $table->integer('responsible_id');
+            $table->integer('stage_id');
+            $table->timestamp('deadline')->nullable();
+            $table->tinyInteger('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
