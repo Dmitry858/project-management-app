0fd633243c2c83f454e1a26edfddfec446ca5bec
Dmitry <frontender@inbox.ru>
2022-07-23 11:19:10 +0400

    Edit DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             MemberSeeder::class,
             PermissionSeeder::class,
             RoleSeeder::class,
+            StageSeeder::class,
         ]);
     }
 }
