76a01adb61c4508ed6f9048f57b5ee3fc05759b3
Dmitry <frontender@inbox.ru>
2022-07-24 21:49:38 +0400

    Create Stage Model

---
+++ app/Models/Stage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
