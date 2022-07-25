6f6c68df2cc83ee3dbdbe97947446ac31b93d6a1
Dmitry <frontender@inbox.ru>
2022-07-25 09:48:53 +0400

    Edit Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -19,4 +19,9 @@ class Project extends Model
     {
         return $this->belongsToMany(Member::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
