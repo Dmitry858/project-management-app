8050c6160676edf7239da7404f357f8462aa4f11
Dmitry <frontender@inbox.ru>
2022-07-28 15:28:26 +0400

    Create TaskService

---
+++ app/Services/TaskService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\TaskRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+
+class TaskService
+{
+    protected $taskRepository;
+
+    public function __construct(TaskRepositoryInterface $taskRepository)
+    {
+        $this->taskRepository = $taskRepository;
+    }
+
+    public function getList(array $filter = [])
+    {
+        if (Auth::user()->hasRole('admin'))
+        {
+            return $this->taskRepository->search($filter);
+        }
+        else
+        {
+            $member = Auth::user()->member()->first();
+            if ($member)
+            {
+                $filter['is_active'] = 1;
+                return $this->taskRepository->searchByMember($member->id, $filter);
+            }
+            else
+            {
+                return [];
+            }
+        }
+    }
+}
