2516a4c5fcd13a3c4fb8d8eadd316b2d0112a93b
Dmitry <frontender@inbox.ru>
2022-07-30 12:38:37 +0400

    Edit AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,10 +7,12 @@ use App\Repositories\Interfaces\ProjectRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\Interfaces\MemberRepositoryInterface;
 use App\Repositories\Interfaces\TaskRepositoryInterface;
+use App\Repositories\Interfaces\StageRepositoryInterface;
 use App\Repositories\EloquentProjectRepository;
 use App\Repositories\EloquentUserRepository;
 use App\Repositories\EloquentMemberRepository;
 use App\Repositories\EloquentTaskRepository;
+use App\Repositories\EloquentStageRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -38,5 +40,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(MemberRepositoryInterface::class, EloquentMemberRepository::class);
 
         $this->app->bind(TaskRepositoryInterface::class, EloquentTaskRepository::class);
+
+        $this->app->bind(StageRepositoryInterface::class, EloquentStageRepository::class);
     }
 }
