8222cbe1b96a6dbf630d82be7061063cc42b4742
Dmitry <frontender@inbox.ru>
2022-08-11 11:13:58 +0400

    Create UpdateTaskRequest

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'max:200'],
+            'owner_id' => ['required'],
+            'responsible_id' => ['required'],
+            'stage_id' => ['required'],
+            'project_id' => ['required'],
+        ];
+    }
+}
