e1198273829ee77e0bd7f03697c0b90e9de7780e
Dmitry <frontender@inbox.ru>
2022-08-13 19:58:04 +0400

    Edit routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SettingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +27,9 @@ Route::middleware(['auth'])->group(function() {
     Route::resource('/projects', ProjectController::class);
     Route::resource('/tasks', TaskController::class);
     Route::resource('/members', MemberController::class);
+    Route::resource('/users', UserController::class);
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/settings', [SettingController::class, 'index'])->name('settings')->middleware('permission:edit-settings');
+    Route::post('/settings', [SettingController::class, 'update'])->name('settings.update')->middleware('permission:edit-settings');
 });
