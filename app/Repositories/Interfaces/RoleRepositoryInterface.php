1e514d7778d497cf4a9d5dc11d52aa5f2edd4207
Dmitry <frontender@inbox.ru>
2022-08-18 11:36:32 +0400

    Edit RoleRepositoryInterface

--- app/Repositories/Interfaces/RoleRepositoryInterface.php
+++ app/Repositories/Interfaces/RoleRepositoryInterface.php
@@ -5,4 +5,6 @@ namespace App\Repositories\Interfaces;
 interface RoleRepositoryInterface
 {
     public function getUserRoles(int $userId): array;
+
+    public function search(array $filter = []);
 }
