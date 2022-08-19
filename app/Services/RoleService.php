0bcbe2bf591b61b51a731a0dc170bbaac5384592
Dmitry <frontender@inbox.ru>
2022-08-19 12:38:27 +0400

    Edit RoleService

--- app/Services/RoleService.php
+++ app/Services/RoleService.php
@@ -3,9 +3,22 @@
 namespace App\Services;
 
 use App\Repositories\EloquentRoleRepository;
+use App\Repositories\Interfaces\RoleRepositoryInterface;
 
 class RoleService
 {
+    protected $roleRepository;
+
+    public function __construct(RoleRepositoryInterface $roleRepository)
+    {
+        $this->roleRepository = $roleRepository;
+    }
+
+    public function getList(array $filter = [])
+    {
+        return $this->roleRepository->search($filter);
+    }
+
     public static function hasUserRole(int $userId, string $roleSlug): bool
     {
         $repo = new EloquentRoleRepository;
