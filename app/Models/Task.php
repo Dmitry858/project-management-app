2a61885e4b1d294ce1eb6f81004ce8e140df6448
Dmitry <frontender@inbox.ru>
2022-09-08 14:45:03 +0400

    Add new method getMemberFullName() to the Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Repositories\EloquentMemberRepository;
+use Illuminate\Support\Facades\Cache;
 
 class Task extends Model
 {
@@ -43,5 +45,26 @@ class Task extends Model
     public function responsible()
     {
         return $this->belongsTo(Member::class, 'responsible_id');
+    }
+
+    public function getMemberFullName(int $memberId): string
+    {
+        $fullName = '';
+        if (Cache::has('member_'.$memberId.'_fullname'))
+        {
+            $fullName = Cache::get('member_'.$memberId.'_fullname');
+        }
+        else
+        {
+            $repo = new EloquentMemberRepository;
+            $member = $repo->find($memberId);
+            if ($member)
+            {
+                $fullName = $member->user->name.' '.$member->user->last_name;
+                Cache::put('member_'.$memberId.'_fullname', $fullName, 86400);
+            }
+        }
+
+        return trim($fullName);
     }
 }
