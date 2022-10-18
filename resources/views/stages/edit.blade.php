e91aeffc572afcc353e8bfddb1e82fa4f80bc456
Dmitry <frontender@inbox.ru>
2022-10-18 09:36:27 +0400

    Create stages.edit view

---
+++ resources/views/stages/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('include.flash-error')
+
+    <form class="w-full max-w-lg" method="POST" action="{{ route('stages.update', ['stage' => $stage->id]) }}">
+        @csrf
+        @method('PUT')
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3 mb-6">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+                    @lang('form.label_title')
+                </label>
+                <input class="appearance-none block w-full text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="name" name="name" type="text" value="{{ $stage->name }}" required>
+            </div>
+
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="slug">
+                    @lang('form.label_slug')
+                </label>
+                <input class="appearance-none block w-full text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="slug" name="slug" type="text" value="{{ $stage->slug }}" required>
+            </div>
+        </div>
+
+        <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            @lang('form.save_button')
+        </button>
+        <a class="inline-block border border-blue-600 hover:bg-blue-600 text-blue-600 hover:text-white font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" href="{{ route('stages.index') }}">
+            @lang('buttons.cancel')
+        </a>
+    </form>
+@endsection
