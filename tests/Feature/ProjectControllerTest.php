5ab53bc1b5f11b6a52eb6ea930a90effd1a27a60
Dmitry <frontender@inbox.ru>
2022-10-25 14:33:47 +0400

    Edit ProjectControllerTest

--- tests/Feature/ProjectControllerTest.php
+++ tests/Feature/ProjectControllerTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Permission;
 
 class ProjectControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_index()
     {
         $response = $this->get(route('projects.index'));
@@ -16,8 +20,30 @@ class ProjectControllerTest extends TestCase
 
     public function test_auth_index()
     {
-        $user = User::factory()->make();
+        $user = User::factory()->create();
         $response = $this->actingAs($user)->get(route('projects.index'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get(route('projects.create'));
+
+        $response->assertStatus(404);
+    }
+
+    public function test_create_with_permission()
+    {
+        $user = User::factory()->create();
+        $permission = Permission::create([
+            'name' => 'Создание проектов',
+            'slug' => 'create-projects'
+        ]);
+        $user->permissions()->attach($permission->id);
+
+        $response = $this->actingAs($user)->get(route('projects.create'));
 
         $response->assertStatus(200);
     }
