4fc7f684d07727c8c206efa8636c324ef2d34ee9
Dmitry <frontender@inbox.ru>
2022-11-10 12:18:44 +0400

    Create Invitation model

---
+++ app/Models/Invitation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+class Invitation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'secret_key',
+        'email',
+    ];
+
+    public function isExpired(): bool
+    {
+        return $this->created_at->diffInHours(Carbon::now()) >= 24;
+    }
+}
