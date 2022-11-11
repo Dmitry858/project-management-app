11c2faee208ef5ea7dca99ad4f5d45b1c1feaeb2
Dmitry <frontender@inbox.ru>
2022-11-11 10:23:28 +0400

    Create InvitationRepositoryInterface

---
+++ app/Repositories/Interfaces/InvitationRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface InvitationRepositoryInterface
+{
+    public function find(int $id);
+
+    public function search(array $filter = []);
+
+    public function createFromArray(array $data);
+
+    public function updateSendingStatus(int $id, int $status): bool;
+
+    public function delete(int $id): bool;
+}
