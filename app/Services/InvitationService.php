8c8dec978b68be4506f13d93b2f769166904c553
Dmitry <frontender@inbox.ru>
2022-11-12 13:17:37 +0400

    Create InvitationService

---
+++ app/Services/InvitationService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\InvitationRepositoryInterface;
+
+class InvitationService
+{
+    protected $invitationRepository;
+
+    public function __construct(InvitationRepositoryInterface $invitationRepository)
+    {
+        $this->invitationRepository = $invitationRepository;
+    }
+
+    public function get(int $id)
+    {
+        return $this->invitationRepository->find($id);
+    }
+
+    public function getValidInvitation(int $id)
+    {
+        $invitation = $this->get($id);
+
+        if (!$invitation) return false;
+
+        return $invitation->isExpired() ? false : $invitation;
+    }
+
+    public function getList(array $filter = [])
+    {
+        return $this->invitationRepository->search($filter);
+    }
+}
