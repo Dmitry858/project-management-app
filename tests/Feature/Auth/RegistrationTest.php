e8a3c7f87d66a15736265a8718df0c9fbafec208
Dmitry <frontender@inbox.ru>
2022-11-15 10:24:06 +0400

    Edit RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Support\Str;
+use App\Models\Invitation;
 
 class RegistrationTest extends TestCase
 {
@@ -12,16 +14,33 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered()
     {
-        $response = $this->get('/register');
+        $key = Invitation::create([
+            'secret_key' => Str::random(32),
+            'email' => 'newuser@test.ru',
+        ]);
+
+        $response = $this->get('/register/'.$key->secret_key);
 
         $response->assertStatus(200);
     }
 
+    public function test_registration_screen_can_not_be_rendered()
+    {
+        $response = $this->get('/register/'.Str::random(32));
+
+        $response->assertStatus(404);
+    }
+
     public function test_new_users_can_register()
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $key = Invitation::create([
+            'secret_key' => Str::random(32),
+            'email' => 'newuser@test.ru',
+        ]);
+
+        $response = $this->post('/register/'.$key->secret_key, [
+            'name' => 'New User',
+            'email' => 'newuser@test.ru',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
