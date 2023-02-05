d5bdf8380fa3b5641b29c2869dee8ed9fb7468ca
Dmitry <frontender@inbox.ru>
2023-02-05 15:59:23 +0400

    Edit AttachmentController

--- app/Http/Controllers/AttachmentController.php
+++ app/Http/Controllers/AttachmentController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\AttachmentService;
 
 class AttachmentController extends Controller
 {
+    protected $attachmentService;
+
+    public function __construct(AttachmentService $attachmentService)
+    {
+        $this->attachmentService = $attachmentService;
+    }
+
     public function show($id)
     {
-        return 'Содержимое файла '.$id;
+        $path = $this->attachmentService->getPath($id);
+
+        return $path ? response()->file($path) : abort(404);
     }
 }
