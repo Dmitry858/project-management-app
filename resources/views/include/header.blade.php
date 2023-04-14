bdfdfafb46503a9b369f874b291dbab6e318155b
Dmitry <frontender@inbox.ru>
2023-04-14 09:12:44 +0400

    Add user photo to the header

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -4,7 +4,7 @@
             <div class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
                 <a href="{{ route('dashboard') }}" aria-label="Home">
                     @if(Storage::disk('public')->exists('logo.png'))
-                        <img src="{{ asset('storage/logo.png') }}" class="max-h-9 max-w-xs pl-2" alt="{{ __('header.logo') }}">
+                        <img src="{{ asset('storage/logo.png') }}" class="max-h-9 max-w-xs pl-2 mt-1" alt="{{ __('header.logo') }}">
                     @else
                         <span class="text-3xl pl-2">@lang('header.logo')</span>
                     @endif
@@ -14,11 +14,13 @@
             @include('include.search-form')
 
             <div class="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
-                <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
-                    <li class="flex-1 md:flex-none md:mr-3">
+                <ul class="list-reset flex justify-between flex-1 md:flex-none items-center greeting-wrap">
+                    <li class="flex items-center flex-1 md:flex-none md:mr-3">
+                        <div class="relative inline-block rounded-full overflow-hidden ml-2 user-photo">
+                            <img src="{{ Auth::user()->photoSrc() }}" alt="">
+                        </div>
                         <div class="relative inline-block">
                             <button id="user-dropdown-trigger" class="drop-button text-white py-2 px-2">
-                                <span class="pr-2"><i class="em em-robot_face"></i></span>
                                 @lang('header.greeting'), {{ Auth::user()->name }}
                                 <svg class="h-3 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
