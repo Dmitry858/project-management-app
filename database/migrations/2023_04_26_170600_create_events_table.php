73f0c0ea06b8aaff7497a4bdb3e975c5e46c086a
Dmitry <frontender@inbox.ru>
2023-04-26 21:17:09 +0400

    Add create_events_table migration

---
+++ database/migrations/2023_04_26_170600_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamp('start');
+            $table->timestamp('end');
+            $table->tinyInteger('is_allday')->default(0);
+            $table->tinyInteger('is_private')->default(1);
+            $table->integer('project_id')->nullable();
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
