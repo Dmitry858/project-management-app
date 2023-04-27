5c5f177fea2ea9daae10354798ba8e30af6061c1
Dmitry <frontender@inbox.ru>
2023-04-27 20:13:06 +0400

    Create Event model

---
+++ app/Models/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'start',
+        'end',
+        'is_allday',
+        'is_private',
+        'project_id',
+        'user_id',
+    ];
+}
