5067e3347a316a96ce4201a4e5d13693afea2fce
Dmitry <frontender@inbox.ru>
2023-04-27 20:35:16 +0400

    Create new config file for calendar

---
+++ config/calendar.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'timezoneName' => 'Europe/Moscow',
+    'colors' => [
+        'private' => '#fff13d',
+        'public' => '#2067ff',
+        'project' => '#dc7401',
+    ],
+];
